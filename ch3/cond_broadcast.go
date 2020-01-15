427b7ba8ee1b1d54d02eb8fb5db069994d1bcc14
kaepa3 <kaepa3@gmail.com>
2020-01-15 10:51:51 +0000

    ch3 add 2

---
+++ ch3/cond_broadcast.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Button struct {
+	Clicked *sync.Cond
+}
+
+func main() {
+	button := Button{Clicked: sync.NewCond(&sync.Mutex{})}
+	subscrive := func(c *sync.Cond, fn func()) {
+		var goroutineRunning sync.WaitGroup
+		goroutineRunning.Add(1)
+		go func() {
+			goroutineRunning.Done()
+			c.L.Lock()
+
+			defer c.L.Unlock()
+			c.Wait()
+			fn()
+		}()
+		goroutineRunning.Wait()
+	}
+
+	var clickRegistered sync.WaitGroup
+	clickRegistered.Add(3)
+	subscrive(button.Clicked, func() {
+		fmt.Println("Maximizeing window.")
+		clickRegistered.Done()
+	})
+
+}
