29485efabf7870cc2ecd12d98c3db60eb09c5bb0
kaepa3 <kaepa3@gmail.com>
2020-01-27 08:24:55 +0000

    add ch5

--- ch5/routine.go
+++ ch5/routine.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -55,23 +56,57 @@ func main() {
 			return heartbeat
 		}
 	}
+	doWorkFn := func(done chan interface{}, intList ...int) (startGoroutineFn, <-chan interface{}) {
+		intChanStream := make(chan (<-chan interface{}))
+		intStream := common.Bridge(done, intChanStream)
+		doWork := func(done <-chan interface{}, pulseInterval time.Duration) <-chan interface{} {
+			intStream := make(chan interface{})
+			heartbeat := make(chan interface{})
+			go func() {
+				defer close(intStream)
+				select {
+				case intChanStream <- intStream:
+				case <-done:
+					return
+				}
+				pulse := time.Tick(pulseInterval)
+				for {
+				valueLoop:
+					for _, intVal := range intList {
+						if intVal < 0 {
+							log.Printf("negative value: %v\n", intVal)
+							return
+						}
+						for {
+							select {
+							case <-pulse:
+								select {
+								case heartbeat <- struct{}{}:
+								default:
+								}
+							case intStream <- intVal:
+								continue valueLoop
+							case <-done:
+								return
+							}
+						}
+					}
+				}
+			}()
+			return heartbeat
+		}
+		return doWork, intStream
+	}
 	log.SetOutput(os.Stdout)
 	log.SetFlags(log.Ltime | log.LUTC)
-	doWork := func(done <-chan interface{}, _ time.Duration) <-chan interface{} {
-		log.Println("ward: Hello, I'm irresponsible!")
-		go func() {
-			<-done
-			log.Println("ward: I am halting.")
-		}()
-		return nil
-	}
-	doWorkWithSteward := newSteward(4*time.Second, doWork)
 	done := make(chan interface{})
-	time.AfterFunc(9*time.Second, func() {
-		log.Println("main; halting steward and ward.")
-		close(done)
-	})
-	for range doWorkWithSteward(done, 4*time.Second) {
+	defer close(done)
+
+	doWork, intStream := doWorkFn(done, 1, 2, -1, 3, 4, 5)
+	doWorkWithSteward := newSteward(1*time.Millisecond, doWork)
+	doWorkWithSteward(done, 1*time.Second)
+	for intVal := range common.Take(done, intStream, 6) {
+		fmt.Printf("Received: %v\n", intVal)
 	}
 	log.Println("Done")
 
